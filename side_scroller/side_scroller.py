73eb352335a9f7184ce7b77f5cc54aa1cb163324
Paul Sheridan <paul.sheridan@bigfishgames.com>
2018-08-06 07:19:37 -0700

    run and jump animations working in both directions

--- side_scroller/side_scroller.py
+++ side_scroller/side_scroller.py
@@ -7,20 +7,24 @@ from tilemap import Tilemap
 class App:
     def __init__(self):
         pyxel.init(241, 160, caption='test game')
-        pyxel.image(0).load(0, 0, 'assets/tileset.png')
-        pyxel.image(1).load(0, 0, 'assets/alien.png')
+        pyxel.image(0).load(0, 0, 'assets/tile_test2.png')
+        pyxel.image(1).load(0, 0, 'assets/anim_test2.png')
 
         self.tile_size = 16
-        self.tilemap = Tilemap(self.build_tilemap('assets/mapfile.txt', 'layer 0'))
-        # self.tilemap1 = Tilemap(self.build_tilemap('tileset.txt', 'layer 1'), True)
+        self.tilemap = Tilemap(self.build_tilemap('assets/map_test2.txt', 'layer 0'))
+        self.tilemap1 = Tilemap(self.build_tilemap('assets/map_test2.txt', 'layer 1'), True)
+        self.tilemap2 = Tilemap(self.build_tilemap('assets/map_test2.txt', 'layer 2'), True)
 
-        self.player_h = 12
+        self.player_h = 11
         self.player_w = 8
+
         self.player_x = 72
         self.player_y = -16
         self.player_vx = 0
         self.player_vy = 0
         self.grounded = False
+
+        self.zero_frame = 0
 
         pyxel.run(self.update, self.draw)
 
@@ -70,7 +74,6 @@ class App:
                     coord[1] // self.tile_size
                 ]
                 if self.tilemap.matrix[left_tile[1]][left_tile[0]] != -1:
-                    self.player_vx = 0
                     self.player_x = (left_tile[0] * self.tile_size) + self.tile_size
 
         elif self.player_vx > 0:
@@ -80,7 +83,6 @@ class App:
                     coord[1] // self.tile_size
                 ]
                 if self.tilemap.matrix[right_tile[1]][right_tile[0]] != -1:
-                    self.player_vx = 0
                     self.player_x = (right_tile[0] * self.tile_size) - self.player_w
 
         player_bottom = self.player_y + self.player_h - 1
@@ -111,6 +113,42 @@ class App:
                     self.player_y = ceiling_tile[1] * self.tile_size + self.tile_size
                     break
 
+    def render_tiles(self, tilemap, colkey):
+        # render the tileset based on information in the tilemap and tileset files loaded on boot.
+        for idy, arr in enumerate(tilemap.matrix):
+            for idx, val in enumerate(arr):
+                if val != -1:
+                    tile_x = idx*self.tile_size
+                    tile_y = idy*self.tile_size
+                    sx = (val % self.tile_size) * self.tile_size
+                    sy = (val // (256 // self.tile_size)) * self.tile_size
+                    pyxel.blt(tile_x, tile_y, 0, sx, sy, self.tile_size, self.tile_size, colkey)
+
+    def render_player(self):
+        frame_w = 11
+        frame_x = frame_w * 7
+        if not self.grounded:
+            if self.player_vy >= 0:
+                frame_x = frame_w * 13
+            else:
+                frame_x = frame_w * 12
+        else:
+            if pyxel.btn(pyxel.KEY_A) or pyxel.btn(pyxel.KEY_D):
+                if pyxel.btnp(pyxel.KEY_A) or pyxel.btnp(pyxel.KEY_D):
+                    self.zero_frame = pyxel.frame_count
+                frame_x = frame_w * (((pyxel.frame_count - self.zero_frame) // 4) % 6)
+        if self.player_vx > 0:
+            otn = -1
+        else:
+            otn = 1
+
+        pyxel.blt(
+            self.player_x-(2*otn),
+            self.player_y-5, 1, frame_x, 16,
+            otn*self.player_w+(3*otn),
+            self.player_h+5, 1
+        )
+
     def update(self):
         if pyxel.btnp(pyxel.KEY_ESCAPE):
             pyxel.quit()
@@ -118,22 +156,11 @@ class App:
         self.update_player()
 
     def draw(self):
-        pyxel.cls(12)
-        # render the tileset based on information in the tilemap and tileset files loaded on boot.
-        for idy, arr in enumerate(self.tilemap.matrix):
-            for idx, val in enumerate(arr):
-                if val != -1:
-                    x = idx*self.tile_size # pylint: disable=C0103
-                    y = idy*self.tile_size # pylint: disable=C0103
-                    sx = (val % self.tile_size) * self.tile_size # pylint: disable=C0103
-                    sy = (val // (256 // self.tile_size)) * self.tile_size # pylint: disable=C0103
-                    pyxel.blt(x, y, 0, sx, sy, self.tile_size, self.tile_size)
-        pyxel.blt(self.player_x, self.player_y, 0, 0, 16
-                  if self.player_vy > 0 else 32, 8, 12, 5)
-        pyxel.text(32, 4, 'X: {}'.format(self.player_x), 0)
-        pyxel.text(4, 4, 'Y: {}'.format(self.player_y), 0)
-        pyxel.text(64, 4, "tile: {}".format(self.player_x // 16), 0)
-        pyxel.text(96, 4, "tile: {}".format(self.player_y // 16), 0)
+        pyxel.cls(1)
+        self.render_tiles(self.tilemap, 1)
+        self.render_tiles(self.tilemap2, 1)
+        self.render_player()
+        self.render_tiles(self.tilemap1, 1)
 
 
 App()
